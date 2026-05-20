edcf0ae0c1da51a7760e747e5162ddc09b8b10b4
its-abhi-IT <abhig636@gmail.com>
2026-05-20 11:29:53 +0530

    code cleanup

--- TicketAPI/obj/Debug/net8.0/TicketApi.AssemblyInfo.cs
+++ TicketAPI/obj/Debug/net8.0/TicketApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa770bef51176513278f2dcb4a7823717b6a6eac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
